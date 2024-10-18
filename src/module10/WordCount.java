ed2add174f6b88a7ff5c61c9f56137f1b6058814
xvanausloos <xavier.vanausloos@ladonneeintelligente.com>
2024-10-18 11:41:51 +0200

    feature: module 10

--- src/module10/WordCount.java
+++ src/module10/WordCount.java
@@ -4,11 +4,14 @@ import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.operators.FilterOperator;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.runtime.executiongraph.Execution;
 import org.apache.flink.api.java.tuple.Tuple2;
+import scala.Int;
 
 public class WordCount {
     public static void main(String[] args) throws Exception {
@@ -35,14 +38,25 @@ public class WordCount {
         // return a tuple of (name, 1)
         DataSet<Tuple2<String, Integer>> tokenized = filtered.map(new Tokenizer());
 
-        System.out.println(tokenized.collect());
+        System.out.println("tokenized: " + tokenized.collect());
+        // group by the tuple field "0" and sum up tuplie field 1
+        DataSet<Tuple2<String, Integer>> counts = tokenized.groupBy(0).sum(1);
+        System.out.println(counts.collect());
+        // emit results
+        if (params.has("output")){
+            //counts.writeAsCsv("file:///tmp/wc.csv", "\n", " ", FileSystem.WriteMode.OVERWRITE);
+            counts.writeAsCsv("file:///tmp/wc.csv", "\n", " ", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+            // exec the program
+            env.execute("word count example");
+        }
         
     }
 
-    public static final class Tokenizer
-            implements MapFunction< String, Tuple2 < String, Integer >> {
-        public Tuple2 < String, Integer > map(String value) {
-            return new Tuple2 < String, Integer > (value, Integer.valueOf(1));
+    public static final class Tokenizer implements MapFunction< String, Tuple2 < String, Integer >> {
+
+        @Override
+        public Tuple2<String, Integer> map(String value) throws Exception {
+            return new Tuple2<String, Integer>(value, 1);
         }
     }
 }
